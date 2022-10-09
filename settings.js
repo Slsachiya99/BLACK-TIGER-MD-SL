e77971da7c451849de3530ae15cebd7ee2604b24
Slsachiya99 <115049370+Slsachiya99@users.noreply.github.com>
2022-10-09 12:39:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,16 +54,16 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['94704274987'] //ur owner number
+global.ownername = "𝗦𝗟 𝗦𝗔𝗖𝗛𝗜𝗬𝗔" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.socialm = "GitHub: https://github.com/Slsachiya99/BLACK-SACHIYA-" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['94704274987']
+global.ownertag = '94704274987' //ur tag number
+global.botname = 'SACHIYA  MD V4' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
@@ -73,13 +73,13 @@ global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur 
 global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = fs.readFileSync("https://i.imgur.com/XHhlqtK.jpeg") //ur thumb pic
+global.log0 = fs.readFileSync("https://i.imgur.com/XHhlqtK.jpeg") //ur logo pic
+global.err4r = fs.readFileSync("https://i.imgur.com/XHhlqtK.jpeg") //ur error pic
+global.thumb = fs.readFileSync("https://i.imgur.com/XHhlqtK.jpeg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['94704274987'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
